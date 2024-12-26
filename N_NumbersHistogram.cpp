5639e7ed208eb7a9226b90dd510a2beb7889c0e4
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-26 18:56:30 +0600

    Add files via upload

---
+++ N_NumbersHistogram.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    char c;
+    cin>>c;
+    
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+
+    {
+        for(int j=0;j<arr[i];j++)
+
+        {
+        cout<<c;
+    }
+    cout<<endl;
+    }
+    return 0;
+
+}
