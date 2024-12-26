9d124d949511e0a68e328c9428746af7190119a0
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-26 18:36:29 +0600

    Add files via upload

---
+++ A_Team.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main ()
+{
+    int t,i,count=0;
+    int a,b,c;
+    cin>>t;
+    while(t--)
+    {
+
+        cin>>a>>b>>c
+
+int f=a+b+c;
+
+        {
+            if(f>=2)
+           count++;
+        }
+    }
+        cout<<count<<endl;
+    return 0;
+}
