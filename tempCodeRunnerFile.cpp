b6f3753cf70875ee7f7a118831768383f1014f54
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-26 19:02:58 +0600

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main ()
+{
+    ll a,b,c,d;
+    double=mul1,mul2;
+    cin>>a>>b>>c>>d;
+    mul1=b*log10(a);
+    mul2=d*log10(c);
+    if(mul1>mul2)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
