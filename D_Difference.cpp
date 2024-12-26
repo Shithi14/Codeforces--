e19faee6dc37cf11a338317ae5f064f483932ed4
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-26 18:42:21 +0600

    Add files via upload

---
+++ D_Difference.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    long long int a,b,c,d,diff;
+    cin>>a>>b>>c>>d;
+    diff=(a*b)-(c*d);
+    cout<<"Difference = "<<diff<<endl;
+    return 0;
+
+}
