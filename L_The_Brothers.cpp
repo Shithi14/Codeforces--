0af36083c8f4ce316d255f368131b9d6a22e7e91
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-26 18:53:55 +0600

    Add files via upload

---
+++ L_The_Brothers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string str1,str2,str3,str4;
+    cin>>str1>>str2>>str3>>str4;
+    if(str2==str4)
+    {
+    cout<<"ARE Brothers"<<endl;
+    }
+else
+{
+cout<<"NOT"<<endl;
+}
+return 0;
+}
