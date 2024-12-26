d0fa1d27d52e15b03d1d3ada22b5629342b5c55a
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-26 18:41:15 +0600

    Add files via upload

---
+++ C_Compare.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string str,srr;
+    cin>>str>>srr;
+    if(str<srr)
+    {
+        cout<<str<<endl;
+
+    }
+    else
+    {
+        cout<<srr<<endl;
+    }
+    return 0;
+}
