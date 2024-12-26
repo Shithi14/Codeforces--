6b4dbd113e8d2bbfe5f8ced53d8f54c65a3aae7f
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-26 18:56:43 +0600

    Add files via upload

---
+++ O_Calculator.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a,b;
+    char c;
+    cin>>a>>c>>b;
+    if(c=='+')
+    {
+        cout<<a+b<<endl;
+    }
+     if(c=='-')
+    {
+        cout<<a-b<<endl;
+    }
+     if(c=='*')
+    {
+        cout<<a*b<<endl;
+    }
+     if(c=='/')
+    {
+        cout<<a/b<<endl;
+    }
+    return 0;
+}
